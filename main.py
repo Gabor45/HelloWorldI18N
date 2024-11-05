ea82967eb4f187332b8b7fc1265d95cd21066b5d
Kurucz <h160619@stud.u-szeged.hu>
2024-11-05 17:35:24 +0100

    Added babel.cfg and some changes.

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import gettext
 from jinja2 import Environment, FileSystemLoader
 
+domain = 'messages'
+current_locale = 'en_US'
+locale_path = 'locales/'
+
 # Fordítás betöltése angol nyelvhez (en)
-translations = gettext.translation('messages', localedir='locales', languages=['en'])
-translations.install()
-_ = translations.gettext
+gnu_translations = gettext.translation(
+    domain=domain,
+    localedir=locale_path,
+    languages=[current_locale]
+)
+gnu_translations.install()  # Magically make the _ function globally available
+_ = gettext.gettext
 
 # Jinja2 környezet létrehozása és fordítás hozzáadása
 env = Environment(
     loader=FileSystemLoader('templates'),
     extensions=['jinja2.ext.i18n']
 )
-env.install_gettext_translations(translations)
+env.install_gettext_translations(gnu_translations, newstyle=True)
 
-template = env.get_template('index.html.j2')
-print(template.render())
+template = env.get_template('index.jinja2')
+result = template.render()
+print(result)
+
+# Mi történik pontosan
+# Belenyúlni a programba (extract)
+# ID-k
 
